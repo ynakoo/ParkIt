9fab6607c89572887ccba016b183244dea5962b3
koynaarya <aryakoyna@gmail.com>
2026-02-21 16:25:18 +0530

    update superadmin controller with parking details

---
+++ backend/src/controllers/superAdmin.controller.js
@@ -0,0 +1,113 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+
+const createParkingArea = async (req, res) => {
+  const { name, location, qrCode, amount } = req.body;
+
+  const parking = await prisma.parkingArea.create({
+    data: {
+      name,
+      location,
+      qrCode,
+      amount,
+    }
+  });
+  res.status(201).json(parking);
+};
+
+const getParkingAreas = async (req, res) => {
+  const areas = await prisma.parkingArea.findMany({
+    include:{
+      user:true
+    }
+  });
+  res.json(areas);
+};
+
+const getParkingAreaDetails = async (req, res) => {
+  const { id } = req.params;
+  
+  const area = await prisma.parkingArea.findUnique({
+    where: { id },
+    include: {
+      user: true,
+      drivers: {
+        include: {
+          user: true
+        }
+      },
+      tickets: true
+    }
+  });
+  
+  res.json(area);
+};
+
+const createManager = async (req, res) => {
+  const { name, email, password, parkingAreaId } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const manager = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      role: "MANAGER",
+    }
+  });
+
+  await prisma.parkingArea.update({
+    where: { id: parkingAreaId },
+    data: {
+      managerId: manager.id,
+      status:"ACTIVE"
+    }
+  });
+
+  res.status(201).json({ message: "Manager Created" });
+};
+
+const getManagers = async (req, res) => {
+  const managers = await prisma.user.findMany({
+    where: { role: "MANAGER" }
+  });
+
+  res.json(managers);
+};
+
+const getPendingDrivers = async (req, res) => {
+  const drivers = await prisma.driver.findMany({
+    where: {
+      status: "INACTIVE"
+    },
+    include: {
+      user: true,
+      parkingArea: true
+    }
+  });
+
+  res.json(drivers);
+};
+
+const approveDriver = async (req, res) => {
+  const id = req.params.userId;
+  console.log("PARAM ID:", req.params.userId);
+  await prisma.driver.update({
+    where: { userId: id },
+    data: { status: "AVAILABLE" }
+  });
+
+  res.json({ message: "Driver Approved" });
+};
+
+module.exports = {
+  createParkingArea,
+  getParkingAreas,
+  getParkingAreaDetails,
+  createManager,
+  getManagers,
+  getPendingDrivers,
+  approveDriver
+};
