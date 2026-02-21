31ce82960da60abfb079ee97bbe344297b07b2b5
koynaarya <aryakoyna@gmail.com>
2026-02-21 16:25:12 +0530

    integrate new routes in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,13 +12,28 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
     const users = await prisma.user.findMany()
-    res.json({ users })
+    res.json( users )
+    // await prisma.parkingArea.update({
+    //     where:{
+    //         id:"d170f956-34b4-4419-822b-b07c0913cd91"
+    //     },
+    //     data:{
+    //         status:"ACTIVE"
+    //     }
+    // })
+    // res.send("success")
 })
 
 const authRoutes = require('./routes/auth.routes')
 const adminRoutes = require('./routes/superAdmin.routes')
+const managerRoutes = require('./routes/manager.routes')
+const userRoutes = require('./routes/user.routes')
+const driverRoutes = require('./routes/driver.routes')
 app.use('/api/auth', authRoutes)
 app.use('/api/superAdmin', adminRoutes)
+app.use('/api/manager', managerRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/driver', driverRoutes)
 // app.use((err, req, res, next) => {
 //     console.error("Unhandled Error:", err);
 //     res.status(500).json({
