85c292dfc175a35c3618d17c84a18c7d08cfe20d
Talari-Ashwin-Raj <ashwin@Ashwins-MacBook-Pro-4.local>
2026-03-11 13:29:10 +0530

    resolved an issue

--- backend/src/controllers/superAdmin.controller.js
+++ backend/src/controllers/superAdmin.controller.js
@@ -31,7 +31,7 @@ const getParkingAreas = async (req, res) => {
 
 const getParkingAreaDetails = async (req, res) => {
   const { id } = req.params;
-  
+
   const area = await prisma.parkingArea.findUnique({
     where: { id },
     include: {
@@ -44,17 +44,18 @@ const getParkingAreaDetails = async (req, res) => {
       tickets: true
     }
   });
-  
+
   res.json(area);
 };
 
 const createManager = async (req, res) => {
   const { name, email, password, parkingAreaId } = req.body;
-
+  console.log(req.body)
   const hashedPassword = await bcrypt.hash(password, 10);
-  const m = prisma.user.findUnique({where:{email}})
-  if (m){
-    return res.status(400).json({error:"Manager with this email already exists."})
+  const m = await prisma.user.findUnique({ where: { email } })
+  console.log(m)
+  if (m) {
+    return res.status(400).json({ error: "Manager with this email already exists." })
   }
   const manager = await prisma.user.create({
     data: {
@@ -64,12 +65,12 @@ const createManager = async (req, res) => {
       role: "MANAGER",
     }
   });
-
+  console.log(manager.id)
   await prisma.parkingArea.update({
     where: { id: parkingAreaId },
     data: {
       managerId: manager.id,
-      status:"ACTIVE"
+      status: "ACTIVE"
     }
   });
 
